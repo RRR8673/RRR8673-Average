2d3cf88a1aa804f74b8ea66bad605fd355e62baf
Rafael Renigado <renigado@aol.com>
2023-03-01 03:39:14 +0000

    done

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,11 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double a = 0;
+        for(int i = 0; i < nums.length; i++){
+            a += (double) nums[i];
+        }
+        a /= nums.length;
+        return a;
     }
 }
